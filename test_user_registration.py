9800ed7810ffe369444b80857a5069d4c148a4ee
Deven5656 <devengupta0987@gmail.com>
2024-08-18 21:55:09 +0530

    [Deven] Refactor : Python Program to check Testcases for email input

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,14 +1,14 @@
 '''
     @Author: Deven Gupta
-    @Date: 17-08-2024
+    @Date: 18-08-2024
     @Last Modified by: Deven Gupta
-    @Last Modified time: 17-08-2024 
+    @Last Modified time: 18-08-2024 
     @Title : TestCase
 
 '''
 
 import unittest
-from user_registration import is_valid_name
+from user_registration import is_valid_name, is_valid_email
 
 
 class TestUserValidation(unittest.TestCase):
@@ -22,6 +22,17 @@ class TestUserValidation(unittest.TestCase):
         self.assertFalse(is_valid_name("gupta"))
         self.assertFalse(is_valid_name("DG"))
         self.assertFalse(is_valid_name("Gupta123"))
+
+    def test_is_valid_email(self):
+        # Valid emails
+        self.assertTrue(is_valid_email("devengupta0987@gmail.com"))
+        self.assertTrue(is_valid_email("dev.gupta@gmail.co.in"))
+
+        # Invalid emails
+        self.assertFalse(is_valid_email("deven.gupta@com"))
+        self.assertFalse(is_valid_email("dev.123@in"))
+        self.assertFalse(is_valid_email("dev.09gupta@.com"))
+
 
 
 if __name__ == "__main__":
