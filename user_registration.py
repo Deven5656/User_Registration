352ffb69f89f00ba46de2cee85b49eebbd0d0825
Deven5656 <devengupta0987@gmail.com>
2024-08-18 21:25:46 +0530

    [Deven] Refactor : Python Program to check Last name starts with Cap and has minimum 3 characters

--- user_registration.py
+++ user_registration.py
@@ -22,17 +22,19 @@ def is_valid_name(name):
     pattern = r'^[A-Z][a-zA-Z]{2,}$'
     return bool(re.match(pattern, name))
 
-def validate_user(first_name):
+def validate_user(first_name, last_name):
     """
     Description:
         This function is use to call other functions
     Parameters:
         first_name (str): First name of user
+        last_name (str): Last name of user
     Returns:
-        boolean : True if match else False
+        tuple : containing boolean values
     """
     valid_first = is_valid_name(first_name)
-    return valid_first
+    valid_last = is_valid_name(last_name)
+    return valid_first, valid_last
 
 
 def main():
@@ -40,13 +42,19 @@ def main():
     logger=create_logger('Validate_logger')
 
     first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
     
-    valid_first = validate_user(first_name)
+    valid_first, valid_last = validate_user(first_name, last_name)
 
     if valid_first:
         print(f"First Name: '{first_name}' - Valid")
     else:
         print(f"First Name: '{first_name}' - Invalid")
+
+    if valid_last:
+        print(f"Last Name: '{last_name}' - Valid")
+    else:
+        print(f"Last Name: '{last_name}' - Invalid")
     
     logger.info("Information is Validated")
    
