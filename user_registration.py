8fe6b33d04dcf72a7863a7dd922fd486f2932db3
Deven5656 <devengupta0987@gmail.com>
2024-08-19 12:28:05 +0530

    [Deven] Refactor : Python Program to validate user mobile number

--- user_registration.py
+++ user_registration.py
@@ -35,8 +35,21 @@ def is_valid_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+(?:\.[a-zA-Z0-9._%+-]+)*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return bool(re.match(pattern, email))
 
+def is_valid_mobile(mobile):
+    """
+    Description:
+        This function is use to match the pattern in mobile number
+    Parameters:
+        mobile (str): mobile number of user
+    Returns:
+        boolean : True if match else False
+    """
+ 
+    pattern = r'^\d{2} \d{10}$'
+    return bool(re.match(pattern, mobile))
 
-def validate_user(first_name, last_name, email):
+
+def validate_user(first_name, last_name, email, mobile):
     """
     Description:
         This function is use to call other functions
@@ -44,41 +57,48 @@ def validate_user(first_name, last_name, email):
         first_name (str): First name of user
         last_name (str): Last name of user
         email (str) : mail of user
+        mobile (str): mobile number of user
     Returns:
         tuple : containing boolean values
     """
     valid_first = is_valid_name(first_name)
     valid_last = is_valid_name(last_name)
     valid_email = is_valid_email(email)
-    return valid_first, valid_last, valid_email
+    valid_mobile = is_valid_mobile(mobile)
+    return valid_first, valid_last, valid_email, valid_mobile
 
 
 def main():
         
-    logger=create_logger('Validate_logger')
+    logger=create_logger('User_Registration')
 
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
     email = input("Enter your email: ")
+    mobile = input("Enter your mobile number (e.g., 91 9765859088): ")
     
-    valid_first, valid_last, valid_email = validate_user(first_name, last_name, email)
+    valid_first, valid_last, valid_email, valid_mobile = validate_user(first_name, last_name, email, mobile)
 
     if valid_first:
-        print(f"First Name: '{first_name}' - Valid")
+        logger.info(f"First Name: '{first_name}' - Valid")
     else:
-        print(f"First Name: '{first_name}' - Invalid")
+        logger.info(f"First Name: '{first_name}' - Invalid")
 
     if valid_last:
-        print(f"Last Name: '{last_name}' - Valid")
+        logger.info(f"Last Name: '{last_name}' - Valid")
     else:
-        print(f"Last Name: '{last_name}' - Invalid")
+        logger.info(f"Last Name: '{last_name}' - Invalid")
 
     if valid_email:
-        print(f"Email: '{email}' - Valid")
+        logger.info(f"Email: '{email}' - Valid")
     else:
-        print(f"Email: '{email}' - Invalid")
+        logger.info(f"Email: '{email}' - Invalid")
+
+    if valid_mobile:
+        logger.info(f"Mobile: '{mobile}' - Valid")
+    else:
+        logger.info(f"Mobile: '{mobile}' - Invalid")
     
-    logger.info("Information is Validated")
    
 if __name__ == "__main__":
     main()
