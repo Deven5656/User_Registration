201665881348f2b6b25362a0a91880bf1d4c2431
Deven5656 <devengupta0987@gmail.com>
2024-08-19 12:28:38 +0530

    [Deven] Refactor : Python Program to check Testcases for mobile number input

--- test_user_registration.py
+++ test_user_registration.py
@@ -8,7 +8,7 @@
 '''
 
 import unittest
-from user_registration import is_valid_name, is_valid_email
+from user_registration import is_valid_name, is_valid_email, is_valid_mobile
 
 
 class TestUserValidation(unittest.TestCase):
@@ -29,9 +29,19 @@ class TestUserValidation(unittest.TestCase):
         self.assertTrue(is_valid_email("dev.gupta@gmail.co.in"))
 
         # Invalid emails
-        self.assertFalse(is_valid_email("deven.gupta@com"))
+        self.assertFalse(is_valid_email("deven.gupta@.com"))
         self.assertFalse(is_valid_email("dev.123@in"))
         self.assertFalse(is_valid_email("dev.09gupta@.com"))
+
+    def test_is_valid_mobile(self):
+        # Valid mobile numbers
+        self.assertTrue(is_valid_mobile("91 9765859088"))
+        self.assertTrue(is_valid_mobile("91 1234567899"))
+
+        # Invalid mobile numbers
+        self.assertFalse(is_valid_mobile("919765859088"))
+        self.assertFalse(is_valid_mobile("91 992233778"))
+        self.assertFalse(is_valid_mobile("91 12345678910"))
 
 
 
